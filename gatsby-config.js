13cb5cec69f365d3a5c13b05b7c87b87d2fd6cba
Nils Borgböhmer <nils@strandrover.com>
2018-12-29 20:17:46 +0100

    adding prism for code highlithgting

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,12 @@ module.exports = {
         path: `${__dirname}/src/posts`,
       },
     },
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-prismjs`],
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
